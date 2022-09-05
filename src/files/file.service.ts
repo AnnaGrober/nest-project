0dfc8c63366df37e5d23afe455f54e2fff403b9a
Anna <a.grober@webpractik.ru>
2022-09-05 15:46:19 +0300

    fix(master): настройка редиса
    Refs: master

--- src/files/file.service.ts
+++ src/files/file.service.ts
@@ -14,6 +14,7 @@ export default class FileService {
     ) {
     }
 
+
     public sendFile(@UploadedFile() file: Express.Multer.File, userId) {
         if (this.convert(file) && file !== undefined) {
             const fileToSave = new File();
