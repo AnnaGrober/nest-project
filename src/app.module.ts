99a3ea2458b1084b2ea5e2ee2237786154967398
Anna Grober <a.grober@webpractik.ru>
2022-09-18 19:55:49 +0300

    socket.io

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { FilesModule } from './files/file.module';
-import { HttpModule } from '@nestjs/axios';
-import { UsersModule } from './users/users.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ORMConfig } from '../ormconfig';
-import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from './auth.guard';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {FilesModule} from './files/file.module';
+import {HttpModule} from '@nestjs/axios';
+import { UsersModule } from "./users/users.module";
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {ORMConfig} from '../ormconfig';
+import {APP_GUARD} from '@nestjs/core';
+import {AuthGuard} from './auth.guard';
+import {AppGateway} from './app.gateway';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(ORMConfig),
-    FilesModule,
-    HttpModule,
-    UsersModule,
-  ],
-  controllers: [AppController],
+    imports: [
+        TypeOrmModule.forRoot(ORMConfig),
+        FilesModule,
+        HttpModule,
+        UsersModule,
+    ],
+    controllers: [AppController],
+    providers: [AppGateway],
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/files/file.service.ts
+++ src/files/file.service.ts
@@ -1,85 +1,97 @@
-import {StreamableFile, UploadedFile} from '@nestjs/common';
-import {createReadStream, readdirSync} from 'fs';
-import {Repository} from 'typeorm';
-import {InjectRepository} from '@nestjs/typeorm';
-import {File} from './file.entity';
-import {InjectRedis, Redis} from '@nestjs-modules/ioredis';
+import { StreamableFile, UploadedFile } from "@nestjs/common";
+import { createReadStream, readdirSync } from "fs";
+import { Repository } from "typeorm";
+import { InjectRepository } from "@nestjs/typeorm";
+import { File } from "./file.entity";
+import { InjectRedis, Redis } from "@nestjs-modules/ioredis";
+import { io } from "socket.io-client";
 
 export default class FileService {
-    public files = [];
+  public files = [];
+  private socket;
+  public messages;
 
-    constructor(
-        @InjectRepository(File)
-        private fileRepository: Repository<File>,
-        @InjectRedis() private readonly redis: Redis,
-    ) {
+  constructor(
+    @InjectRepository(File)
+    private fileRepository: Repository<File>,
+    @InjectRedis() private readonly redis: Redis
+  ) {
+    this.socket = io("http://localhost:3000");
+    this.socket.on("msgToClient", (message) => {
+      this.receivedMessage(message);
+    });
+  }
+
+  receivedMessage(message) {
+    this.messages.push(message);
+  }
+
+  public async sendFile(@UploadedFile() file: Express.Multer.File, userId) {
+    await this.redis.lpush(
+      "history_" + userId.toString(),
+      JSON.stringify({
+        user_id: userId,
+        act: "пользователь отправил файл",
+        date: new Date(Date.now()).toLocaleString(),
+      })
+    );
+    if (this.convert(file) && file !== undefined) {
+      await this.redis.lpush(
+        "history_" + userId.toString(),
+        JSON.stringify({
+          user_id: userId,
+          act: "пользователь конвертировал файл",
+          date: new Date(Date.now()).toLocaleString(),
+        })
+      );
+      const fileToSave = new File();
+      fileToSave.userId = userId;
+      fileToSave.name = file.originalname;
+
+      this.saveFile(fileToSave);
+      this.socket.emit("Файл успешно сконвертировался!");
+      return file.originalname;
     }
+  }
 
-    public async sendFile(@UploadedFile() file: Express.Multer.File, userId) {
-        await this.redis.lpush(
-            'history_' + userId.toString(),
-            JSON.stringify({
-                user_id: userId,
-                act: 'пользователь отправил файл',
-                date: new Date(Date.now()).toLocaleString(),
-            }),
-        );
-        if (this.convert(file) && file !== undefined) {
-            await this.redis.lpush(
-                'history_' + userId.toString(),
-                JSON.stringify({
-                    user_id: userId,
-                    act: 'пользователь конвертировал файл',
-                    date: new Date(Date.now()).toLocaleString(),
-                }),
-            );
-            const fileToSave = new File();
-            fileToSave.userId = userId;
-            fileToSave.name = file.originalname;
+  public async getFiles(userId) {
+    await this.redis.lpush(
+      "history_" + userId.toString(),
+      JSON.stringify({
+        user_id: userId,
+        act: "пользователь получил файлы",
+        date: new Date(Date.now()).toLocaleString(),
+      })
+    );
+    return this.fileRepository.find({
+      where: {
+        userId: userId,
+      },
+    });
+  }
 
-            this.saveFile(fileToSave);
-            return file.originalname;
-        }
-    }
+  public saveFile(file: File) {
+    this.fileRepository.save(file);
+  }
 
-    public async getFiles(userId) {
-         await this.redis.lpush(
-             'history_' + userId.toString(),
-             JSON.stringify({
-                 user_id: userId,
-                 act: 'пользователь получил файлы',
-                 date: new Date(Date.now()).toLocaleString(),
-             }),
-         );
-        return this.fileRepository.find({
-            where: {
-                userId: userId,
-            },
-        });
-    }
+  public getFile(name: string): StreamableFile {
+    const filePath = `uploads/${name}`;
+    const file = createReadStream(filePath);
+    return new StreamableFile(file);
+  }
 
-    public saveFile(file: File) {
-        this.fileRepository.save(file);
-    }
+  public getAllFiles() {
+    this.throughDirectory("uploads");
+    return this.files;
+  }
 
-    public getFile(name: string): StreamableFile {
-        const filePath = `uploads/${name}`;
-        const file = createReadStream(filePath);
-        return new StreamableFile(file);
-    }
+  private throughDirectory(directory: string) {
+    readdirSync(directory).forEach((file) => {
+      return this.files.push(file);
+    });
+  }
 
-    public getAllFiles() {
-        this.throughDirectory('uploads');
-        return this.files;
-    }
-
-    private throughDirectory(directory: string) {
-        readdirSync(directory).forEach((file) => {
-            return this.files.push(file);
-        });
-    }
-
-    private convert(@UploadedFile() file: Express.Multer.File) {
-        return true;
-    }
+  private convert(@UploadedFile() file: Express.Multer.File) {
+    return true;
+  }
 }
